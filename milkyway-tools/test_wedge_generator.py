Fixed radians/degrees issue in wedge generator and astro_coordinates.  All transforms now take degrees as input and produce outputs in degrees.  Wedge generator now properly sends theta and phi coordinates as degrees.
 Please enter the commit message for your changes. Lines starting

milkyway-tools/test_wedge_generator.py
@@ -15,6 +15,8 @@ import astro_coordinates as co
 """This script creates test stripes for stream mle code and milkyway@home.
 Matthew Newby (RPI), March 28, 2011
 """
+deg = 180.0 / ma.pi 
+rad = ma.pi / 180.0 
 
 # Finish stream_length, convolve, and master function
 
@@ -116,7 +118,7 @@ def stream_into_stripe(params, sn, N_stars, batch=1000, fileout="streamgen82.txt
         u,v,w = generate_stream(batch, u_min, u_max, sigma)
         holder = []
         for i in range(len(u)):
-            mu1, nu1, r1 = co.streamToGC(u[i],v[i],w[i],mu,R,theta,phi,wedge)
+            mu1, nu1, r1 = co.streamToGC(u[i],v[i],w[i],mu,R,theta*deg,phi*deg,wedge)
             if (nu1 < nu_min) or (nu1 > nu_max):  nu_killed=nu_killed+1; continue
             if (mu_max > 360.0):
                 if (mu1 > (mu_max-360.0)) and (mu1 < mu_min):  mu_killed=mu_killed+1; continue
@@ -156,8 +158,8 @@ def get_stream_length(params, N=0, accuracy=0.0001):
     u1,v1,w1 = 0.1,  0.0, 0.0
     u2,v2,w2 = -0.1,  0.0, 0.0
     #check to see which is closest to the nu = +2.5 boundry, flip if necessary
-    mu1, nu1, r1 = co.streamToGC(u1,v1,w1,mu, R, theta, phi, wedge)
-    mu2, nu2, r2 = co.streamToGC(u2,v2,w2,mu, R, theta, phi, wedge)
+    mu1, nu1, r1 = co.streamToGC(u1,v1,w1,mu, R, theta*deg, phi*deg, wedge)
+    mu2, nu2, r2 = co.streamToGC(u2,v2,w2,mu, R, theta*deg, phi*deg, wedge)
     if np.fabs(nu1 - 2.5) > np.fabs(nu2 - 2.5):
         u1, u2 = (-1.0*u1), (-1.0*u2)
         temp = nu1
@@ -165,7 +167,7 @@ def get_stream_length(params, N=0, accuracy=0.0001):
     # check them against wedge boundries (mu, nu, r)
     test = 0
     while test==0:
-        mu1, nu1, r1 = co.streamToGC(u1,v1,w1,mu, R, theta, phi, wedge)
+        mu1, nu1, r1 = co.streamToGC(u1,v1,w1,mu, R, theta*deg, phi*deg, wedge)
         # account for wrap-around
         if params.mu_lim[1] > 360.0:
             if mu1 < params.mu_lim[0]:  mu1 = mu1 + 360.0
@@ -187,7 +189,7 @@ def get_stream_length(params, N=0, accuracy=0.0001):
     # Do for u2
     test = 0
     while test==0:
-        mu2, nu2, r2 = co.streamToGC(u2,v2,w2,mu, R, theta, phi, wedge)
+        mu2, nu2, r2 = co.streamToGC(u2,v2,w2,mu, R, theta*deg, phi*deg, wedge)
         # account for wrap-around
         if params.mu_lim[1] > 360.0:
             if mu2 < params.mu_lim[0]:  mu2 = mu2 + 360.0

paper-tools/sgrWide_utils.py
@@ -15,8 +15,9 @@ import glob
 def make_sim_stream_plot():
     """ Makes the plot for the simulated streams 
         /home/newbym2/Dropbox/Research/sgrnorth_paper/sgr_separated_stars_MRT.txt"""
-    filename="streamgen_bifExtra.txt"
-    file2="streamgen_sgr_sim.txt"
+    folder = "/home/newbym2/Dropbox/Research/sgrLetter/"
+    filename=folder+"streamgen_bifExtra.txt"
+    file2=folder+"streamgen_sgr_sim.txt"
     data = np.loadtxt(filename)
     for i in range(len(data[:,0])):
         data[i,0], data[i,1] = ac.lbToEq(data[i,0], data[i,1])
@@ -74,23 +75,29 @@ def make_total_plot(path="/home/newbym2/Desktop/starfiles"):
 
 def make_diff_hist():
     """ Subtracts the simulated streams from the real data """
+    folder = "/home/newbym2/Dropbox/Research/sgrLetter/"
     data = np.loadtxt("SDSSnorth.csv", delimiter=",")
+    sims = ["streamgen_bifGood.csv", "streamgen_bif50Good.csv", "streamgen_bifExtra.csv", "streamgen_bifShift.csv"]
+    outs = ["streamgen_ccdiff.png", "streamgen_cc50good.png", "streamgen_ccExtra.png", "streamgen_ccShift.png"]
     #sim = np.loadtxt("simtotbif50m30_radec.csv", delimiter=",")
     #sim = np.loadtxt("simtotbifm2_radec.csv", delimiter=",")
     #sim = np.loadtxt("streamgen_bifExtra.csv", delimiter=",")
-    sim = np.loadtxt("streamgen_bifGood.csv", delimiter=",")
+    #sim = np.loadtxt("streamgen_bifGood.csv", delimiter=",")
     #sim = np.loadtxt("streamgen_bif50Good.csv", delimiter=",")
-    for i in range(sim.shape[0]):
-        for j in range(sim.shape[1]):
-            if data[i,j] < 1.0:  sim[i,j] = 0.0
-    sky = pp.HistMaker([1,2], [1,2], xsize=0.5, ysize=0.5, 
-        xarea=(120.0, 250.0), yarea=(-10.0, 50.0))
-    #new = sim #data - sim
-    new = data - sim
-    sky.H = new
-    sky.cmap = 'color_c'
-    sky.varea = (-100.0,100.0)
-    pp.PlotHist(sky, "streamgen_ccdiff.png")
+    for z in range(len(sims)):
+        sim = np.loadtxt(folder+sims[z], delimiter=",")
+        for i in range(sim.shape[0]):
+            for j in range(sim.shape[1]):
+                if data[i,j] < 1.0:  sim[i,j] = 0.0
+        sky = pp.HistMaker([1,2], [1,2], xsize=0.5, ysize=0.5, 
+            xarea=(120.0, 250.0), yarea=(-10.0, 50.0))
+        #new = sim #data - sim
+        new = data - sim
+        sky.H = new
+        sky.cmap = 'color'
+        sky.varea = (0.0,200.0)
+        pp.PlotHist(sky, "sim_"+outs[z])
+        #pp.PlotHist(sky, "data.png"); break
     #pp.PlotHist(sky, "streamgen_bif50Good_bwdiff.png")
     print "All done"
    

plotting-tools/plot_pack.py
@@ -30,6 +30,36 @@ cdict1 = {'red':  ((0.0, 1.0, 1.0),
 white_black = LinearSegmentedColormap('grey', cdict1)
 # Custom central-void (white) color map
 cdict2 = {'red':  ((0.0, 0.0, 0.0),
+                   (0.125, 1.0, 1.0),
+                   (0.25, 0.0, 0.0),
+                   (0.4, 0.5, 0.5),                   
+                   (0.5, 1.0, 1.0),
+                   (0.6, 0.0, 0.0),
+                   (0.7, 1.0, 1.0),
+                   (0.875, 0.75, 0.75),
+                   (1.0, 0.5, 0.5)),
+         'green': ((0.0, 0.0, 0.0),
+                   (0.125, 0.0, 0.0),
+                   (0.25, 0.0, 0.0),
+                   (0.4, 0.5, 0.5),
+                   (0.5, 1.0, 1.0),
+                   (0.6, 1.0, 1.0),
+                   (0.7, 1.0, 1.0),
+                   (0.875, 0.0, 0.0),
+                   (1.0, 0.2, 0.2)),
+         'blue':  ((0.0, 0.0, 0.0),
+                   (0.125, 1.0, 1.0),
+                   (0.25, 1.0, 1.0),
+                   (0.4, 1.0, 1.0),
+                   (0.5, 1.0, 1.0),
+                   (0.6, 0.0, 0.0),
+                   (0.7, 0.0, 0.0),
+                   (0.825, 0.0, 0.0),
+                   (1.0, 0.2, 0.2))        }
+spec_center = LinearSegmentedColormap('spec_c', cdict2)
+
+# IN PROGRESS
+cdict3 = {'red':  ((0.0, 0.0, 0.0),
                    (0.5, 1.0, 1.0),
                    (1.0, 1.0, 1.0)),
          'green': ((0.0, 0.0, 0.0),
@@ -38,7 +68,7 @@ cdict2 = {'red':  ((0.0, 0.0, 0.0),
          'blue':  ((0.0, 0.0, 0.0),
                    (0.5, 1.0, 1.0),
                    (1.0, 0.0, 0.0))        }
-spec_center = LinearSegmentedColormap('spec_c', cdict2)
+#spec_center = LinearSegmentedColormap('spec_c', cdict3)
 #Custom heatmap
 spectral_colors = np.loadtxt('../utilities/spectral_cm.txt')
 spectral_wb = ListedColormap(spectral_colors[:,:3], name="spectral_wb", N=256)