e96020ac7712c65e4469785fe153f5fa097e03b6
M Newby <matthew.t.newby@gmail.com>
2019-09-05 17:57:39 -0400

    Update calc_start.py
    Python3 update

--- utilities/calc_start.py
+++ utilities/calc_start.py
@@ -4,21 +4,21 @@ Matthew Newby, July 11, 2011'''
 
 
 from math import *
-print "# Imported all of math directly to shell"
+print("# Imported all of math directly to shell")
 import numpy as np
-print "# Imported numpy as np"
+print("# Imported numpy as np")
 import scipy as sc
-print "# imported scipy as sc"
+print("# imported scipy as sc")
 import scipy.constants as con
-print "# Imported scipy.constants as con"
+print("# Imported scipy.constants as con")
 #import files as fi
 #print "# imported files as fi"
 import matplotlib
 import matplotlib.pyplot as plt
-print "# imported matplotlib.pyplot as plt"
+print("# imported matplotlib.pyplot as plt")
 try:
     import astro_coordinates as co
-    print "# imported astro_coordinates as co"
+    print("# imported astro_coordinates as co")
 except ImportError:
-    print "!!! Unable to import astro_coordinates"
-print "\n# Entering Interactive Session:"
+    print("!!! Unable to import astro_coordinates")
+print("\n# Entering Interactive Session:")
